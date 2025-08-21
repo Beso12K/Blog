d02206c588143fea0dc5870feb2a5a893951f737
Beso12K <kadekmahesa99@gmail.com>
2025-08-21 20:53:37 +0800

    Masuk

---
+++ js.js
@@ -0,0 +1,6 @@
+function fungsi1() {
+  document.getElementsByClassName("dipakai")[0].style.textDecoration = "none";
+}
+function fungsi2() {
+  document.getElementsByClassName("dipakai")[0].style.textDecoration = "underline";
+}
